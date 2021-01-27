02d2cea5f2225b6e0c8b874ae5d4aff26c941376
Sander Ruscigno <ruscigno@gmail.com>
2021-01-26 22:19:52 -0600

    feat: minikube as service

--- raspberry-pi-4/minikube-install.sh
+++ raspberry-pi-4/minikube-install.sh
@@ -1,35 +1,41 @@
-#sudo useradd -s /bin/bash -d /home/sander/ -m -G sudo sander
-#sudo passwd sander
+#useradd -s /bin/bash -d /home/sander/ -m -G sander
+#passwd sander
 #su - sander
+#passwd -l ubuntu
 
-sudo apt update
-sudo apt upgrade -y
-sudo apt autoremove
-sudo apt-get install -y \
+apt update
+apt upgrade -y
+apt autoremove
+apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
    "deb [arch=arm64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-sudo usermod -aG docker $USER
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+usermod -aG docker $USER
+newgrp docker
+newgrp sander
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-arm64
-sudo install minikube-linux-arm64 /usr/local/bin/minikube
-sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2 curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
-sudo swapoff -a
+install minikube-linux-arm64 /usr/local/bin/minikube
+apt-get update && apt-get install -y apt-transport-https gnupg2 curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubectl
+swapoff -a
 minikube config set driver docker
 minikube config set memory 6544
 minikube config set cpus 4
-minikube start
-
+#minikube start
+cp minikube.service /etc/systemd/system/
+systemctl enable minikube
+#systemctl start minikube
+echo REBOOT-REBOOT-REBOOT
