d1f26081054393209202ab82787369c53142bf26
Sander Ruscigno <ruscigno@gmail.com>
2021-01-26 19:32:45 -0600

    feat: ubuntu server minikube install script

---
+++ raspberry-pi-4/README.md
@@ -0,0 +1,21 @@
+## Required Hardware
+- Raspberry Pi 4, with 4GB or 8GB RAM — the 4GB is the best value, 8GB is best if you don’t plan on doing clustering.
+- SD card — 32GB recommended, but keep in mind that Kubernetes writes to disk a lot and it could kill a card.
+- Power supply — you need the official supply or you can use PC Power Supply as I did.
+
+## Flash OS
+You can use [Raspebian](https://www.raspberrypi.org/software/) ([64bits](https://downloads.raspberrypi.org/raspios_arm64/images/)), [Ubuntu Server](https://ubuntu.com/download/raspberry-pi) or [Ubuntu Core](https://ubuntu.com/download/iot#core) (IOT optimized).
+
+I would recommend you to install a 64b OS version..
+
+Flash it to the SD card.
+
+## Pre-requisites
+- Change the password with `passwd pi` or `passwd ubuntu`.
+- Run `raspi-config` and change the memory split to `16mb`, so that we have all the RAM for Kubernetes.
+- I also recommend to disable HDMI to save more memory and probably lower the temperature in some components.
+
+### Essential step
+- Add the following to `/boot/cmdline.txt`, but make sure that you don’t add new lines.
+`cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory`
+
---
+++ raspberry-pi-4/minikube-install.sh
@@ -0,0 +1,32 @@
+sudo useradd -s /bin/bash -d /home/sander/ -m -G sudo sander
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=arm64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-arm64
+sudo install minikube-linux-arm64 /usr/local/bin/minikube
+sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2 curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+sudo swapoff -a
+minikube config set driver docker
+minikube config set memory 6544
+minikube config set cpus 4
+minikube start
+
